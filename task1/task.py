1dab3b11c0e204ad863c87424f62ae711b3702be
ZzlodeyY <semensiemens@yandex.ru>
2024-10-29 14:37:21 +0300

    add main function

--- task1/task.py
+++ task1/task.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def get_char(csv_path:str,row_index : int, col_index:int):
+def main(csv_path:str,row_index : int, col_index:int):
     df = pd.read_csv(csv_path)
     return df.iloc[row_index].values[col_index]
 
-print(get_char("Путь к csv"))
+print(main("Путь к csv"))
